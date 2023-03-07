486b14f568ca3b57f3e4334c2b291dedabb373c8
xuezhennnn <88291141+xuezhennnn@users.noreply.github.com>
2023-03-07 09:35:00 +0800

    Create 27_快慢指针.cpp

---
+++ 27_快慢指针.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int size = nums.size();
+        int slow = 0;
+        for(int fast = 0; fast < size; fast++){
+            if(nums[fast] != val){
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
+
+};
